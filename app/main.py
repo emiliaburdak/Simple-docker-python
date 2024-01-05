912771bc2c16e70c491604baf155ca50bcd79149
Em <emiliaburdak@gmail.com>
2024-01-05 13:26:26 +0100

    the program exit code has been relay to the parent process

--- app/main.py
+++ app/main.py
@@ -15,12 +15,17 @@ def main():
     # print(completed_process.stdout.decode("utf-8"))
 
     # Execute with command, args but capture out and err
+    # communicate() reads all the processes inputs and outputs and stores it in the variables
     completed_process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = completed_process.communicate()
 
     # it works like print
     sys.stdout.write(stdout.decode("utf-8"))
     sys.stderr.write(stderr.decode("utf-8"))
+
+    # process that has been executed by .run or .popen has attribute .returncode that is the number
+    # witch indicates success of the process, sys.exit ends the process and pass the result to the initial environment
+    sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
